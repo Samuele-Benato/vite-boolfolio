483a8aea5ed67af156a1f2220aa354c6ef871b38
Samuele-Benato <samuele.benato@gmail.com>
2023-11-03 17:18:09 +0100

    feat : add card

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ProjectList from './project/ProjectList.vue';
+
+export default {
+    components: {
+      ProjectList,
+  },
+}; 
+</script>
+
+<template>
+  <ProjectList/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/project/ProjectCard.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  props: {
+    project: Object, 
+  },
+};
+</script>
+
+<template>
+  
+        <div class="row g-3">
+            <div class="col-4 col-lg-3 col-md-6 col-sm-12 ">
+                <img class="img-fluid" :src="project.image" alt="project img">
+            </div>
+            <div class="col-8 col-lg-9 col-md-6 col-sm-12 d-flex align-items-center">
+                <div class="row g-3 ">
+                    <div class="col-1">
+                        <strong>Id: </strong> {{ project.id }}
+                    </div>
+                    <div class="col-3">
+                        <strong>Titolo: </strong> {{ project.title }}
+                    </div>
+                    <div class="col-4">
+                        <strong>Tipo: </strong>
+                        {{ project.type ? project.type.label : 'Nessun type' }}
+                    </div>
+                    <div class="col-4">
+                        <strong>Tecnologia: </strong>
+                        <div v-for="( technology in project.technologies)"></div>
+                            {{ technology.label }} 
+                    </div>
+                    <div class="col-12">
+                        <strong>Descrizione: </strong> {{ project.description }}
+                    </div>
+                    <div class="col-12">
+                        <strong>Link: </strong> {{ project.link }}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</template>
+
+<style lang="scss">
+ 
+</style>
