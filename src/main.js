0ca233ae7788351007a1364712940213bd035687
Samuele-Benato <samuele.benato@gmail.com>
2023-11-03 17:17:52 +0100

    feat : add start structure

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.scss'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import App from './App.vue'
+
+createApp(App).mount('#app')
