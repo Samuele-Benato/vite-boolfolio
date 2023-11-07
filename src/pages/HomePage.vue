d918301f735daedfbc61682feb375ee05c281328
Samuele-Benato <samuele.benato@gmail.com>
2023-11-07 16:25:39 +0100

    feat : add navbar

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<script>
+
+
+export default {
+  data() {
+    return {
+    }
+  },
+
+  components: {
+    
+  },
+};
+</script>
+
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<style lang="scss" scoped></style>
