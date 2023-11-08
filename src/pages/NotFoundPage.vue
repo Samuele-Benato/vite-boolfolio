bfa5fa1f894e9a0ed9352b0868f9c0efaec8f72e
Samuele-Benato <samuele.benato@gmail.com>
2023-11-08 14:37:46 +0100

    feat : add NotFoundPage

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Progetto non trovato"
+    }
+  }
+};
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="my-3">{{ title }}</h1>
+        <router-link                    
+            :to="{
+            name: 'portfolio',
+            }" 
+            class="btn btn-dark ">
+            Ritorna alla lista
+        </router-link>
+    </div>            
+</template>
+
+<style lang="scss" scoped></style>
