4851cec83c8fa3f125f620338a37d11407551f82
Samuele-Benato <samuele.benato@gmail.com>
2023-11-08 14:38:58 +0100

    fix : projectDetail

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -6,6 +6,10 @@ export default {
   data() {
     return {
       project : {},
+      error : {
+      any : false,
+      message:'',
+      }
     }
   },
 
@@ -16,7 +20,6 @@ export default {
  },
 
   created() {
-    
     axios
       .get('http://127.0.0.1:8000/api/projects/' + this.$route.params.id)
       .then((response) => {
@@ -24,6 +27,9 @@ export default {
       })
       .catch((error) => {
         console.error('Errore nella chiamata API', error);
+        this.$router.push({name : 'not-found'})
+        // this.error.any = true;
+        // this.error.message = error.response.data.message;
       });
   },
 };
@@ -32,6 +38,9 @@ export default {
 <template>
     <div class="container my-3">
         <h2>Dettaglio Progetto</h2>
+        <!-- <div class="alert alert-danger" v-if="error.any">
+          {{ error.message }}
+        </div> -->
         <div class="d-flex align-items-center">
           <img class="float-start me-2 mb-3 w-50" :src="'http://127.0.0.1:8000/storage/' + project.image" alt="project img">
           <ProjectCard :project="project" :isDetail="true" v-if="project"/>
